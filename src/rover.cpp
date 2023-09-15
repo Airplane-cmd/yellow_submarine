4e87231d4fe8d06c55f75632a1c568b1e29c3b12
Sapsay Nikita <simonpatrikoff1@outlook.com>
2023-09-15 20:01:09 +1000

    last

--- src/rover.cpp
+++ src/rover.cpp
@@ -7,21 +7,40 @@
 #include <errno.h> // Error integer and strerror() function
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+
+
 
 #include <string>
 #include <vector>
 #include <sstream>
+#include <array>
 
 using std::placeholders::_1;
 class Rover : public rclcpp::Node
 {
   private:
     bool debug_f = 0;
+
+    int serial_port;
+    struct termios tty;
+
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_full;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr vo_pose;
+
+    std::array<double, 3> m_position;
+    std::array<double, 3> m_rotation;
+    std::array<double, 3> m_positionGoal_arr;
+    std::array<double, 3> m_rotationGoal_arr;
+
     void topic_callback(const std_msgs::msg::String &msg) const
     {
       RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg.data.c_str());
@@ -59,13 +78,57 @@ class Rover : public rclcpp::Node
       if(!debug_f)	write(serial_port, command.c_str(), command.size());
 
     }
-    int serial_port;
-    struct termios tty;
+    void topic_callback_getRPY(const geometry_msgs::msg::PoseStamped &msg)
+    {
+      RCLCPP_INFO(this->get_logger(), "[Pose] Received something from slam node");
+
+      std::array<double, 4> q;
+      
+      q[0] = msg.pose.orientation.x;
+      q[1] = msg.pose.orientation.y;
+      q[2] = msg.pose.orientation.z;
+      q[3] = msg.pose.orientation.w;
+      
+      m_rotation[0] = std::atan2(2.0 * (q[0] * q[1] + q[3] * q[2]), pow(q[3], 2) - pow(q[0], 2) - pow(q[1], 2) + pow(q[2], 2));//roll //not sure if it's working
+      m_rotation[1] = std::asin(-2.0 * (q[0] * q[2] - q[3] * q[1]));//pitch
+      m_rotation[2] = std::atan2(2.0 * (q[0] * q[1] + q[3] * q[2]), pow(q[3], 2) + pow(q[0], 2) - pow(q[1], 2) - pow(q[2], 2));//yaw
+
+      m_position[0] = msg.pose.position.x;
+      m_position[1] = msg.pose.position.y;
+      m_position[2] = msg.pose.position.z;
+      
+      std::array<double, 4> data;
+      data[0] = m_rotation[2];
+      for(uint8_t i = 0; i < 3; ++i)
+      {
+	data[i + 1] = m_position[i];
+      }
+      std::string command_str;
+      for(uint8_t i = 0; i < 4; ++i)
+      {
+	float num = static_cast<float>(data[i]);
+	char arr[4];
+	memcpy(arr, &num, 4);
+	command_str += std::string(arr, 4);
+      }
+      for(double it : data)	std::cout << it << '\n';
+      std::string prefix = {char(45), char(231)};
+      
+      sendString(prefix, command_str);
+    }
+    
+    void sendString(const std::string &prefix, const std::string &data)
+    {
+      std::string command = prefix + data + '\n';
+      if(!debug_f)	write(serial_port, command.c_str(), command.size());
+    }
+
   public:
     Rover() : Node("rover")
     {
       subscription_ = this->create_subscription<std_msgs::msg::String>("navigation_topic", 10, std::bind(&Rover::topic_callback, this, _1));
       subscription_full = this->create_subscription<std_msgs::msg::String>("hr_topic", 10, std::bind(&Rover::topic_callback_with_parsing, this, _1));
+      vo_pose = this->create_subscription<geometry_msgs::msg::PoseStamped>("/visual_slam/tracking/vo_pose", 10, std::bind(&Rover::topic_callback_getRPY, this, _1));
 
       this->declare_parameter("debug", "Nope");
       std::string debugStatus_str= this->get_parameter("debug").as_string();
@@ -117,7 +180,7 @@ class Rover : public rclcpp::Node
       cfsetospeed(&tty, B9600);
 
   // Save tty settings, also checking for error
-      if (tcsetattr(serial_port, TCSANOW, &tty) != 0 && !debug_f) 
+      if(tcsetattr(serial_port, TCSANOW, &tty) != 0 && !debug_f) 
       {
 	RCLCPP_INFO(this->get_logger(), "[-] Tcgetattr error, exiting with errno '%i'", errno);
 	rclcpp::shutdown();
